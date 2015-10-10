d3db246a123efdfa428cc4d5a2933d52e04eb65f
Pavel Andrienko <pasha.ne@gmail.com>
2015-10-10 23:54:21 +0300

    Fixed error

--- src/RandomForestMP.java
+++ src/RandomForestMP.java
@@ -9,7 +9,6 @@ import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.regression.LabeledPoint;
 import org.apache.spark.mllib.tree.model.RandomForestModel;
 import org.apache.spark.mllib.tree.RandomForest;
-import org.apache.spark.mllib.util.MLUtils;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
@@ -18,6 +17,32 @@ import java.util.HashMap;
 import java.util.regex.Pattern;
 
 public final class RandomForestMP {
+
+   private static class ParseTrainingData implements Function<String, LabeledPoint> {
+        private static final Pattern SPACE = Pattern.compile(",");
+
+        public LabeledPoint call(String line) {
+            String[] tok = SPACE.split(line);
+            double[] point = new double[tok.length-1];
+            for (int i = 1; i < tok.length; ++i) {
+                point[i-1] = Double.parseDouble(tok[i]);
+            }
+            return LabeledPoint(Double.parseDouble(tok[0]), Vectors.dense(point));
+        }
+    }
+
+    private static class ParseTestData implements Function<String, Vector> {
+        private static final Pattern SPACE = Pattern.compile(",");
+
+        public Vector call(String line) {
+            String[] tok = SPACE.split(line);
+			double[] point = new double[tok.length-1];
+            for (int i = 1; i < tok.length; ++i) {
+                point[i-1] = Double.parseDouble(tok[i]);
+            }
+            return Vectors.dense(point);			
+        }
+    }
 
     public static void main(String[] args) {
         if (args.length < 3) {
@@ -33,9 +58,12 @@ public final class RandomForestMP {
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 		
 		// Load and parse the data files
-		JavaRDD<LabeledPoint> trainingData = MLUtils.loadLibSVMFile(sc.sc(), training_data_path).toJavaRDD();
+		JavaRDD<String> lines_training_data = sc.textFile(training_data_path);
+		JavaRDD<String> lines_test_data = sc.textFile(test_data_path);
 		
-		JavaRDD<LabeledPoint> testData = MLUtils.loadLibSVMFile(sc.sc(), test_data_path).toJavaRDD();
+		
+		JavaRDD<LabeledPoint> trainingData = lines_training_data.map(new ParseTrainingData());
+		JavaRDD<Vector> testData = lines_test_data.map(new ParseTestData());
 		
 		// Train a RandomForest model
         Integer numClasses = 2;
@@ -49,7 +77,7 @@ public final class RandomForestMP {
 
 		final RandomForestModel model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo, numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins, seed);
 		
-		// Evaluate model on test instances and compute test error
+/*		// Evaluate model on test instances and compute test error
 		JavaPairRDD<Double, Double> predictionAndLabel =
 		  testData.mapToPair(new PairFunction<LabeledPoint, Double, Double>() {
 			@Override
@@ -66,7 +94,7 @@ public final class RandomForestMP {
 		  }).count() / testData.count();
 		System.out.println("Test Error: " + testErr);
 		System.out.println("Learned classification forest model:\n" + model.toDebugString());		
-/*
+*/
         JavaRDD<LabeledPoint> results = testData.map(new Function<Vector, LabeledPoint>() {
             public LabeledPoint call(Vector points) {
                 return new LabeledPoint(model.predict(points), points);
@@ -74,7 +102,7 @@ public final class RandomForestMP {
         });
 
         results.saveAsTextFile(results_path);
-*/
+
         sc.stop();
     }
 
